23287cf9405f47cfe9008c23c808fc0df2f1f2a8
juanstcp26 <juanstcp26@gmail.com>
2022-04-25 12:44:24 -0500

    Ingresar Uusario completo

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+using ProyectoDSWI.Entity;
+using ProyectoPidG01.Models;
+using ProyectoDSWI.Models;
+using ProyectoDSWI.Filters;
+
+namespace ProyectoPidG01.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private DB_PIGrupo01Entities1 db = new DB_PIGrupo01Entities1();
+
+        UsuarioDAO objUsu = new UsuarioDAO();
+        
+        public ActionResult Create()
+        {
+            ViewBag.roles = new SelectList(db.usp_RolListar(), "id", "nombre");
+            return View("Create", "_Layout",new usuario());
+        }
+
+        [HttpPost]
+        public ActionResult Create(usuario reg)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    objUsu.InsertarUsuario(reg);
+                    return RedirectToAction("Create");
+                }
+                return RedirectToAction("Create");
+            }
+            catch
+            { return View(); }
+        }
+    }
+}
---
+++ Entity/Usuario1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDSWI.Entity
+{
+    public class Usuario1
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateTime fecha { get; set; }
+        public int idRol { get; set; }
+    }
+}
