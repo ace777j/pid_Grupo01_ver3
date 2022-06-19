0403d2c471c240ea443da38a96a7a4bf93c0375d
juanstcp26 <juanstcp26@gmail.com>
2022-06-19 03:03:54 -0500

    Todo Incidentes

--- Entity/Incidente1.cs
+++ Entity/Incidente1.cs
@@ -13,16 +13,20 @@ namespace ProyectoDSWI.Entity
         public int idIncidente { get; set; }
 
         [Required(ErrorMessage = "Esta celda es necesaria de completar")]
+        [DisplayName("Comentario")]
         public string comentario { get; set; }
 
         [Required(ErrorMessage = "Esta celda es necesaria de completar")]
+        [DisplayName("Departamento")]
         public int idDepa { get; set; }
 
         [Required(ErrorMessage = "Esta celda es necesaria de completar")]
+        [DisplayName("Causa")]
         public int idCausa { get; set; }
         public string descripcionC { get; set; }
 
         [Required(ErrorMessage = "Esta celda es necesaria de completar")]
+        [DisplayName("Estado")]
         public int idEstado { get; set; }
         public string descripcionE { get; set; }
     }
